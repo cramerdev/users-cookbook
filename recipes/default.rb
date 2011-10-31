45c6d8567ed67137d9c3bda02de12dfa1cf8f001
Nathan L Smith <nlloyds@gmail.com>
2011-10-31 11:54:11 -0500

    Handle locked users better.

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,34 +20,42 @@ sysadmin_group = Array.new
 
 search(:users) do |u|
   # Move on to the next user if this one is not specified for this node
-  # TODO: Lock the user if they are not included on this node or specified to
-  # be locked (#9823)
-  next if u[:nodes].is_a?(Array) && !u[:nodes].include?(node.name)
+  next if u[:nodes].is_a?(Array) && !u[:nodes].include?(node.name) && !u['locked']
 
   sysadmin_group << u['id'] if u["groups"].include?("admin")
 
   home_dir = "/home/#{u['id']}"
 
-  user u['id'] do
-    uid u['uid']
-    shell u['shell'] || "/bin/bash"
-    comment u['comment']
-    supports :manage_home => true
-    home home_dir
-  end
+  # Try locking a user
+  if u['locked']
+    user u['id'] do
+      action :lock
+      only_if "grep #{u['id']} /etc/passwd"
+    end
+  # Or create/update one
+  else
+    user u['id'] do
+      uid u['uid']
+      shell u['shell'] || "/bin/bash"
+      comment u['comment']
+      supports :manage_home => true
+      home home_dir
+      action u['locked'] ? :lock : :create
+    end
 
-  directory "#{home_dir}/.ssh" do
-    owner u['id']
-    group u['id']
-    mode "0700"
-  end
+    directory "#{home_dir}/.ssh" do
+      owner u['id']
+      group u['id']
+      mode "0700"
+    end
 
-  template "#{home_dir}/.ssh/authorized_keys" do
-    source "authorized_keys.erb"
-    owner u['id']
-    group u['id']
-    mode "0600"
-    variables :ssh_keys => u['ssh_keys'].join("\n")
+    template "#{home_dir}/.ssh/authorized_keys" do
+      source "authorized_keys.erb"
+      owner u['id']
+      group u['id']
+      mode "0600"
+      variables :ssh_keys => u['ssh_keys'].join("\n")
+    end
   end
 
   (u['groups'] || []).each do |g|
